b0d336c5652650716c9c5dcac7f4bc0743d09b7d
gurveersingh8806 <gurveersingh8806@gmail.com>
2025-10-02 23:57:15 +0530

    Add yoo.cpp practice file

---
+++ C++/yoo.cpp
@@ -0,0 +1,29 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int i = 1;
+    while(i<=n){
+        int k=1;
+        while(k<=n-i+1){
+            cout<<k;
+            k++;
+        }
+        int star = 1;
+        while(star<i){
+            cout <<"**";
+            star++;
+        }
+        int j =n-i+1;
+        while (j){
+            cout <<j;
+            j--;
+        }
+        cout << endl;
+        i++;
+    }
+}
+
